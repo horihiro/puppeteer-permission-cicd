ec3e9035c9ffc35468e3a157b1f061948d9aa65c
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2021-12-06 19:34:20 +0900

    Add `mount` function

---
+++ showMounts/index.js
@@ -0,0 +1,9 @@
+const execAsync = require('../utils/execAsync');
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  context.res = {
+    body: await execAsync('mount')
+  }
+}
